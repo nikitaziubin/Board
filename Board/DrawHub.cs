fa9f23c49ea427fc738368094ad63f2f1f370a31
nikita.ziubin <nikita.ziubin@gmail.com>
2023-10-29 22:36:52 +0200

    Add project files.

---
+++ Board/DrawHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+public class DrawHub : Hub
+{
+	public async Task SendDrawing(object data)
+	{
+		await Clients.Others.SendAsync("ReceiveDrawing", data);
+	}
+	public async Task ClearBoard()
+	{
+		await Clients.All.SendAsync("ClearBoard");
+	}
+	
+
+}
---
+++ Board/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder => builder
+		.AllowAnyOrigin()
+		.AllowAnyHeader()
+		.AllowAnyMethod());
+});
+
+
+builder.Services.AddSignalR();
+
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts(); 
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();	
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseCors(); 
+
+
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+	DefaultFileNames = new List<string> { "Board.html" }
+});
+app.UseStaticFiles();
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapHub<DrawHub>("/drawHub");
+});
+
+app.Run();
