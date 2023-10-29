5119ec42ec332646a629587e1d6575e8c19d1216
nikita.ziubin <nikita.ziubin@gmail.com>
2023-10-29 22:47:21 +0200

    Board with web sockets and  Long Poll

---
+++ Board/ConnectionMapping.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class ConnectionMapping<T>
+{
+	private readonly Dictionary<T, HashSet<string>> _connections = new Dictionary<T, HashSet<string>>();
+	private readonly Dictionary<string, TaskCompletionSource<object>> _tcsConnections = new Dictionary<string, TaskCompletionSource<object>>();
+
+	public void Add(T key, string connectionId, TaskCompletionSource<object> tcs)
+	{
+		lock (_connections)
+		{
+			if (!_connections.TryGetValue(key, out var connections))
+			{
+				connections = new HashSet<string>();
+				_connections.Add(key, connections);
+			}
+
+			connections.Add(connectionId);
+		}
+
+		lock (_tcsConnections)
+		{
+			_tcsConnections[connectionId] = tcs;
+		}
+	}
+
+	public void Remove(string connectionId)
+	{
+		lock (_connections)
+		{
+			foreach (var key in _connections.Keys)
+			{
+				if (_connections[key].Contains(connectionId))
+				{
+					_connections[key].Remove(connectionId);
+					break;
+				}
+			}
+		}
+
+		lock (_tcsConnections)
+		{
+			_tcsConnections.Remove(connectionId);
+		}
+	}
+
+	public IEnumerable<string> GetConnections(T key)
+	{
+		if (_connections.TryGetValue(key, out var connections))
+		{
+			return connections;
+		}
+
+		return new List<string>();
+	}
+
+	public IEnumerable<string> GetConnections()
+	{
+		List<string> allConnections = new List<string>();
+		foreach (var key in _connections.Keys)
+		{
+			allConnections.AddRange(_connections[key]);
+		}
+		return allConnections;
+	}
+
+	public TaskCompletionSource<object> GetTcs(string connectionId)
+	{
+		if (_tcsConnections.TryGetValue(connectionId, out var tcs))
+		{
+			return tcs;
+		}
+
+		return null;
+	}
+}
--- Board/DrawHub.cs
+++ Board/DrawHub.cs
@@ -2,14 +2,37 @@
 
 public class DrawHub : Hub
 {
+	private readonly static ConnectionMapping<string> _connections = new ConnectionMapping<string>();
 	public async Task SendDrawing(object data)
 	{
 		await Clients.Others.SendAsync("ReceiveDrawing", data);
 	}
+
 	public async Task ClearBoard()
 	{
 		await Clients.All.SendAsync("ClearBoard");
 	}
-	
 
+	public async Task WaitForDrawing()
+	{
+		var tcs = new TaskCompletionSource<object>();
+		_connections.Add(Context.ConnectionId, Context.ConnectionId, tcs);
+
+		await tcs.Task;
+	}
+
+	public override Task OnDisconnectedAsync(Exception exception)
+	{
+		_connections.Remove(Context.ConnectionId);
+		return base.OnDisconnectedAsync(exception);
+	}
+
+	public async Task TriggerDrawing(object data)
+	{
+		await Clients.All.SendAsync("ReceiveDrawing", data);
+		foreach (var connectionId in _connections.GetConnections())
+		{
+			_connections.GetTcs(connectionId).SetResult(null);
+		}
+	}
 }
